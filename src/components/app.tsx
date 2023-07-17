a53a556ba259d3b594b24cf9aa8ee698322f2b1d
Pedro Arruda <pedroarruda91@gmail.com>
2023-07-17 18:09:15 -0300

    base do projeto completado

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,8 +2,15 @@
 
 import React, { useState, useEffect } from "react";
 
-const App = () => {
-  const [gastos, setGastos] = useState({
+type Gasto = {
+  descricao: string;
+  valor: number;
+};
+
+type GastosPorDia = Record<string, Gasto[]>;
+
+const App: React.FC = () => {
+  const [gastos, setGastos] = useState<GastosPorDia>({
     "segunda-feira": [],
     "terça-feira": [],
     "quarta-feira": [],
@@ -12,16 +19,17 @@ const App = () => {
     "sábado": [],
     "domingo": [],
   });
-  const [diaDaSemana, setDiaDaSemana] = useState("segunda-feira");
+
+  const [diaDaSemana, setDiaDaSemana] = useState<string>("segunda-feira");
 
   useEffect(() => {
     const data = localStorage.getItem("gastos");
     if (data) {
-      setGastos(JSON.parse(data));
+      setGastos(JSON.parse(data) as GastosPorDia);
     }
   }, []);
 
-  const adicionarGasto = (descricao: any, valor: any) => {
+  const adicionarGasto = (descricao: string, valor: number) => {
     setGastos((prevGastos) => {
       return {
         ...prevGastos,
@@ -33,7 +41,7 @@ const App = () => {
   const exibirGastos = (dia: string) => {
     return (
       <ul>
-        {gastos[dia].map((gasto: { descricao: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; valor: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | React.PromiseLikeOfReactNode | null | undefined; }, index: React.Key | null | undefined) => (
+        {gastos[dia].map((gasto, index) => (
           <li key={index}>
             {gasto.descricao} - {gasto.valor}
             <button onClick={() => deletarGasto(dia, index)}>Deletar</button>
@@ -43,7 +51,7 @@ const App = () => {
     );
   };
 
-  const deletarGasto = (dia: string | number, index: number) => {
+  const deletarGasto = (dia: string, index: number) => {
     setGastos((prevGastos) => {
       const updatedGastos = [...prevGastos[dia]];
       updatedGastos.splice(index, 1);
@@ -62,21 +70,29 @@ const App = () => {
     <div>
       <h1>Controlador financeiro</h1>
       <form
-        onSubmit={(e) => {
+        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
           e.preventDefault();
-          adicionarGasto(e.target.descricao.value, e.target.valor.value);
+          const form = e.target as HTMLFormElement;
+          adicionarGasto(form.descricao.value, +form.valor.value);
+          form.reset();
         }}
       >
-        <input type="text" placeholder="Descrição" name="descricao" />
+        <select name="descricao">
+          <option value="Contas">Contas</option>
+          <option value="Alimentação">Alimentação</option>
+          <option value="Lazer">Lazer</option>
+          <option value="Farmácia">Farmácia</option>
+          <option value="Outros">Outros</option>
+        </select>
         <input type="number" placeholder="Valor" name="valor" />
         <input type="submit" value="Adicionar gasto" />
       </form>
       <ul className="tabs">
         {Object.keys(gastos).map((dia) => (
           <li key={dia}>
-            <a href="#" onClick={() => setDiaDaSemana(dia)}>
+            <button onClick={() => setDiaDaSemana(dia)}>
               {dia.charAt(0).toUpperCase() + dia.slice(1)}
-            </a>
+            </button>
           </li>
         ))}
       </ul>
